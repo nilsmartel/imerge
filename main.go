e16626bdf2bbef946cc44548914449c4d66d6b96
Nils Martel <akantorabc@yahoo.de>
2019-06-07 14:21:40 +0200

    Set up Basic Layout of Main Window

--- main.go
+++ main.go
@@ -1,35 +1,66 @@
 package main
 
 import (
-	"fyne.io/fyne"
+	// "fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
-func showHelp(app fyne.App) {
-	helpText := "Use IMerge to blend multiple images evenly into one. First, select the folder in which all your Images are stored. Press 'Go' and all images in that directory will be merged into one"
-	w := app.NewWindow("Help")
-
-	w.SetContent(widget.NewLabel(helpText))
-
-	w.ShowAndRun()
-}
-
 func main() {
 	app := app.New()
 
+	quit := func() { _ = app.Quit }
+
+	showHelp := func() {
+		helpText := "Use IMerge to blend multiple images evenly into one. First, select the folder in which all your Images are stored. Press 'Go' and all images in that directory will be merged into one"
+
+		w := app.NewWindow("Help")
+		w.SetContent(widget.NewLabel(helpText))
+
+		w.ShowAndRun()
+	}
+
+	//  TODO merge Image Funciton
+	mergeImages := quit
+
+	//  TODO Show Files
+	showFiles := quit
+
+	// TODO Select directory funciotn
+	selectDirectory := quit
+
 	w := app.NewWindow("IMerge")
-	w.SetContent(widget.NewVBox(
-		widget.NewLabel("IMerge"),
-		widget.NewHBox(
-			widget.NewButton("Quit", func() {
-				app.Quit()
-			}),
-			widget.NewButton("Help", func() {
-				showHelp(app)
-			}),
-		),
-	))
+
+	directoryLabel := widget.NewLabel("Directory")
+
+	amountLabel := widget.NewLabel("Amount: 0")
+
+	dimensionLabel := widget.NewLabel("Dimension:\n width: 0px\n height: 0px")
+
+	w.SetContent(
+		widget.NewVBox(
+			widget.NewLabel("IMerge"),
+			// TODO maybe a little space (~16px) here?
+			directoryLabel,
+			widget.NewHBox(
+				widget.NewButton("Select", selectDirectory),
+				widget.NewButton("Show", showFiles),
+			),
+
+			widget.NewGroup(
+				"Info",
+				widget.NewVBox(
+					amountLabel,
+					dimensionLabel,
+				),
+			),
+
+			widget.NewButton("Go", mergeImages),
+			widget.NewHBox(
+				widget.NewButton("Quit", quit),
+				widget.NewButton("Help", showHelp),
+			),
+		))
 
 	w.ShowAndRun()
 }
