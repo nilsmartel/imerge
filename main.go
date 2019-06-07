547b5f7a135d4b6908dbab636d4a789efcfcc96b
Nils Martel <akantorabc@yahoo.de>
2019-06-07 13:38:39 +0200

    First steps in fyne GUI Development taken

--- main.go
+++ main.go
@@ -1,29 +1,35 @@
 package main
 
 import (
-	"fmt"
-
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
 )
 
+func showHelp(app fyne.App) {
+	helpText := "Use IMerge to blend multiple images evenly into one. First, select the folder in which all your Images are stored. Press 'Go' and all images in that directory will be merged into one"
+	w := app.NewWindow("Help")
+
+	w.SetContent(widget.NewLabel(helpText))
+
+	w.ShowAndRun()
+}
+
 func main() {
-	defer fmt.Println("Quitting App")
-
-	fmt.Println("starting app")
-
 	app := app.New()
 
-	w := app.NewWindow("Hello")
+	w := app.NewWindow("IMerge")
 	w.SetContent(widget.NewVBox(
-		widget.NewLabel("Hello Fyne!"),
-		widget.NewButton("Quit", func() {
-			app.Quit()
-		}),
+		widget.NewLabel("IMerge"),
+		widget.NewHBox(
+			widget.NewButton("Quit", func() {
+				app.Quit()
+			}),
+			widget.NewButton("Help", func() {
+				showHelp(app)
+			}),
+		),
 	))
 
-	fmt.Println("show Window")
 	w.ShowAndRun()
-
-	fmt.Println("Window running")
 }
