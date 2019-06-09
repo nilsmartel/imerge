d29fc939716ac783d3cef746ede786aa0e492b89
Nils Martel <akantorabc@yahoo.de>
2019-06-09 19:49:25 +0200

    Implement Logic for Listing Files

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
+	"path/filepath"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/app"
 	"fyne.io/fyne/widget"
@@ -36,8 +39,51 @@ func showDialog(app fyne.App, title, message string) {
 	w.Show()
 }
 
+func cutOff(s string, length int) string {
+	if len(s) < length {
+		return s
+	}
+	return s[:length] + "..."
+}
+
 func main() {
 	app := app.New()
+	directoryLabel := widget.NewLabel("Directory")
+	amountLabel := widget.NewLabel("Amount: 0")
+	dimensionLabel := widget.NewLabel("Dimension:\n width: 0px\n height: 0px")
+
+	directory := ""
+	var images []string
+
+	setDirectory := func(dir string) {
+		{
+			directoryFiles, err := ioutil.ReadDir(dir)
+
+			if err != nil {
+				showDialog(app, "Error", "Failed to read Directory "+dir)
+				return
+			}
+
+			directory = dir
+			directoryLabel.SetText(cutOff(directory, 32))
+
+			images = make([]string, 0)
+			isImage := func(s string) bool {
+				switch s {
+				case ".png", ".jpg", ".jpeg":
+					return true
+				default:
+					return false
+				}
+			}
+
+			for _, file := range directoryFiles {
+				if !file.IsDir() && isImage(filepath.Ext(file.Name())) {
+					images = append(images, file.Name())
+				}
+			}
+		}
+	}
 
 	quit := app.Quit
 
@@ -47,25 +93,15 @@ func main() {
 	//  TODO Show Files
 	showFiles := quit
 
-	// TODO Select directory funciotn
 	selectDirectory := func() {
 		directory, err := dialog.Directory().Title("Select Images").Browse()
-
+		// err might be nil in case uses clicks 'cancel'. In that case he woun't even be surprised, if it's not working
 		if err != nil {
-			showDialog(app, "Error", "Couldn't open Directory Browser")
-		} else {
-			showDialog(app, "Success", directory)
+			setDirectory(directory)
 		}
-
 	}
 
 	w := app.NewWindow("IMerge")
-
-	directoryLabel := widget.NewLabel("Directory")
-
-	amountLabel := widget.NewLabel("Amount: 0")
-
-	dimensionLabel := widget.NewLabel("Dimension:\n width: 0px\n height: 0px")
 
 	w.SetContent(
 		widget.NewVBox(
